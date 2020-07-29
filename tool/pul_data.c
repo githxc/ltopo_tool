5a05973e284e9dd7a7aeb2d95bc8047651e1436f
githxc <964654684@qq.com>
2020-07-29 14:26:10 +0800

    1、取消了长脉冲与短脉冲的区分 2、增加了判断窗口开始和结尾的宏 3、增加了第一个数据与最后一个数据的JUMP_FL的宏，当定义此宏时，判断数据缺失时会跳过第一个和最后一个数据

--- tool/pul_data.c
+++ tool/pul_data.c
@@ -9,28 +9,50 @@ int pul_detect(long int *data)
 	int d_value;
 	n = 0;
 
-	for(i=1;i<7;i++)
+	#ifdef JUMP_FL
+	for(i=(BEGIN_WINDOW);i<END_WINDOW-1;i++)
 	{
+		//DETECT DATA LOSS 
+		if(data[i]==-1)
+			return -1;
+	}	
+	#else
+	for(i=(BEGIN_WINDOW-1);i<END_WINDOW;i++)
+	{
+		//DETECT DATA LOSS 
 		if(data[i]==-1)
 			return -1;
 	}
+	#endif
 
 	
-	for(i=1;i<8;i++)
-	{
-		if((i==1)&&(data[0]==-1))
+	for(i=BEGIN_WINDOW;i<END_WINDOW;i++)
+	{		
+		#ifdef JUMP_FL
+		//if the first number is -1
+		if((i==BEGIN_WINDOW)&&(data[BEGIN_WINDOW-1]==-1))
 			continue;
-		d_value = data[i]-data[i-1];
+		#endif
 		
+		d_value = data[i]-data[i-1];
+
 		if(d_value>0)
 		{
 			if(d_value > UP_THRESHOLD_VALUE)
 			{
+				//stage_up return 31,pulse_up return pul_length*2
 				while(d_value > UPE_THRESHOLD_VALUE)
 				{
 					n++;
 					d_value = data[i+n]-data[i+n-1];
-					if((n+i==8)||(data[i+n]==-1))
+
+					#ifdef JUMP_FL
+					//if the last number is -1
+					if(data[i+n]==-1)
+						return 31;
+					#endif
+					
+					if(n+i==END_WINDOW)
 						return 31;
 				}
 				return 2*n;
@@ -40,16 +62,23 @@ int pul_detect(long int *data)
 		{
 			if(d_value < DOWN_THRESHOLD_VALUE)
 			{
+				//stage_down return 30,pulse_down return pul_length*2+1
 				while(d_value < DOWNE_THRESHOLD_VALUE)
 				{
 					n++;
 					d_value = data[i+n]-data[i+n-1];
-					if((n+i==8)||(data[i+n]==-1))
+
+					#ifdef JUMP_FL
+					//if the last number is -1
+					if(data[i+n]==-1)
+						return 31;
+					#endif
+					
+					if(n+i==END_WINDOW)
 						return 30;
 				}
 				return 2*n+1;
 			}
-			
 		}
 	}
 	return 0;
@@ -59,46 +88,32 @@ int pul_detect(long int *data)
 int pul_judg(int pul_num)
 {
 	int u_or_d;
-	int pul_l;
+	//int pul_l;
 
-	
 	if(pul_num == 31)
 		return 2;
 	else if (pul_num == 30)
 		return 3;
 	else if(pul_num == -1)
 		return -1;
-
-	if(pul_num) //如果传回来的值不是30或31或-1
+	else if(pul_num == 0)
+		return 4;
+	
+	//exclude 31,30,-1,detect pul_length
+	if(pul_num) 
 	{
 		if(pul_num%2)
 		{
 			u_or_d = 0;
-			pul_l = pul_num/2;
+			//pul_l = pul_num/2;
 		}
 		else
 		{
 			u_or_d = 1;
-			pul_l = pul_num/2;
+			//pul_l = pul_num/2;
 		}
 	}
-	else if(pul_num == 0)
-	{
-		pul_l = 0;
-		u_or_d = 4;
-	}
-
-	if(pul_l <= S_PUL_LENGTH)
-	{
-		return u_or_d;
-	}
-	else
-	{
-		if(u_or_d)
-			return 5;
-		else
-			return 6;
-	}
+	return u_or_d;
 }
 
 
@@ -115,54 +130,43 @@ int pul_out_m(int pulse,char *msg, FILE *fpo)
 		fprintf(fpo,"%s%s\n", LTOOL_STR_STAGE_DOWN,msg);
 	else if(pulse==4)
 		fprintf(fpo, "%s%s\n", LTOOL_STR_STILL,msg);
-	else if(pulse==5)
-		fprintf(fpo,"%s%s\n",LTOOL_STR_LPULS_UP,msg);
-	else if(pulse==6)
-		fprintf(fpo,"%s%s\n",LTOOL_STR_LPULS_DOWN,msg);
 	else 
 		fprintf(fpo, "%s%s\n", LTOOL_STR_LOSS,msg);
 	return 1;
 }
+
 int pul_out_d(int pulse,int d_pulse,char *msg, FILE *fpo)
 {
-		if(pulse == d_pulse)
-		{
-			if(d_pulse==1)
-				fprintf(fpo, "%s%s\n", LTOOL_STR_PULS_UP,msg);
-			else if(d_pulse==0)
-				fprintf(fpo, "%s%s\n", LTOOL_STR_PULS_DOWN,msg);
-			else if(d_pulse==2)
-				fprintf(fpo, "%s%s\n", LTOOL_STR_STAGE_UP,msg);
-			else if(d_pulse==3)
-				fprintf(fpo, "%s%s\n", LTOOL_STR_STAGE_DOWN,msg);
-			else if(d_pulse==4)
-				fprintf(fpo, "%s%s\n", LTOOL_STR_STILL,msg);
-			else if(d_pulse==5)
-				fprintf(fpo,"%s%s\n",LTOOL_STR_LPULS_UP,msg);
-			else if(d_pulse==6)
-				fprintf(fpo,"%s%s\n",LTOOL_STR_LPULS_DOWN,msg);
-			else
-				fprintf(fpo, "%s%s\n", LTOOL_STR_LOSS,msg);
-		}
+	if(pulse == d_pulse)
+	{
+		if(d_pulse==1)
+			fprintf(fpo, "%s%s\n", LTOOL_STR_PULS_UP,msg);
+		else if(d_pulse==0)
+			fprintf(fpo, "%s%s\n", LTOOL_STR_PULS_DOWN,msg);
+		else if(d_pulse==2)
+			fprintf(fpo, "%s%s\n", LTOOL_STR_STAGE_UP,msg);
+		else if(d_pulse==3)
+			fprintf(fpo, "%s%s\n", LTOOL_STR_STAGE_DOWN,msg);
+		else if(d_pulse==4)
+			fprintf(fpo, "%s%s\n", LTOOL_STR_STILL,msg);
 		else
-		{
-			if(d_pulse==1)
-				fprintf(fpo, "%s%s  %s\n", LTOOL_STR_PULS_UP,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==0)
-				fprintf(fpo, "%s%s  %s\n", LTOOL_STR_PULS_DOWN,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==2)
-				fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STAGE_UP,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==3)
-				fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STAGE_DOWN,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==4)
-				fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STILL,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==5)
-				fprintf(fpo,"%s%s  %s\n",LTOOL_STR_LPULS_UP,msg,LTOOL_STR_ERROR);
-			else if(d_pulse==6)
-				fprintf(fpo,"%s%s  %s\n",LTOOL_STR_LPULS_DOWN,msg,LTOOL_STR_ERROR);
-			else
-				fprintf(fpo, "%s%s\n", LTOOL_STR_LOSS,msg);
-		}
+			fprintf(fpo, "%s%s\n", LTOOL_STR_LOSS,msg);
+	}
+	else
+	{
+		if(d_pulse==1)
+			fprintf(fpo, "%s%s  %s\n", LTOOL_STR_PULS_UP,msg,LTOOL_STR_ERROR);
+		else if(d_pulse==0)
+			fprintf(fpo, "%s%s  %s\n", LTOOL_STR_PULS_DOWN,msg,LTOOL_STR_ERROR);
+		else if(d_pulse==2)
+			fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STAGE_UP,msg,LTOOL_STR_ERROR);
+		else if(d_pulse==3)
+			fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STAGE_DOWN,msg,LTOOL_STR_ERROR);
+		else if(d_pulse==4)
+			fprintf(fpo, "%s%s  %s\n", LTOOL_STR_STILL,msg,LTOOL_STR_ERROR);
+		else
+			fprintf(fpo, "%s%s\n", LTOOL_STR_LOSS,msg);
+	}
 	return 1;
 }
 
